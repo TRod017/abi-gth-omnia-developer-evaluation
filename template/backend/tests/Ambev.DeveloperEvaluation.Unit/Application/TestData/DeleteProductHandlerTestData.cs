7934c7cb9222f40eef311f587e920c83c50ad2ce
Thiago Rodolfo <trodolfo@gmail.com>
2025-05-15 12:13:43 -0300

    test(product): adicionar testes unitários para DeleteProductHandler

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/TestData/DeleteProductHandlerTestData.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.DeleteProduct;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application.TestData;
+
+/// <summary>
+/// Provides test data for DeleteProductHandler unit tests.
+/// </summary>
+public static class DeleteProductHandlerTestData
+{
+    private static readonly Faker<DeleteProductCommand> faker = new Faker<DeleteProductCommand>()
+        .RuleFor(p => p.Id, f => f.Random.Guid());
+
+    public static DeleteProductCommand GenerateValidCommand() => faker.Generate();
+}
