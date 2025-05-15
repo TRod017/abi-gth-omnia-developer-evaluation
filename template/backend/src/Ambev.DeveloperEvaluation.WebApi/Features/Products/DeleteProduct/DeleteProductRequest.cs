2a93b8f8ff8a4e65a28eaf5785e9c5eaaf495ed5
Thiago Rodolfo <trodolfo@gmail.com>
2025-05-15 12:02:24 -0300

    feat(product): adicionar suporte à exclusão de produto na WebApi
    - Criado DeleteProductRequest contendo o identificador do produto
    - Implementado DeleteProductRequestValidator com validação do ID

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Products/DeleteProduct/DeleteProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.DeleteProduct;
+
+/// <summary>
+/// Request for deleting a product by ID.
+/// </summary>
+public class DeleteProductRequest
+{
+    public Guid Id { get; set; }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Products/DeleteProduct/DeleteProductRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.DeleteProduct;
+
+/// <summary>
+/// Validator for DeleteProductRequest.
+/// </summary>
+public class DeleteProductRequestValidator : AbstractValidator<DeleteProductRequest>
+{
+    public DeleteProductRequestValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
