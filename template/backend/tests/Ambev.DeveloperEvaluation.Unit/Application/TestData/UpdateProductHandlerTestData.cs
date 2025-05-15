f4980f92bf50de56f30e60cc56e9bbb8c9da6f1d
Thiago Rodolfo <trodolfo@gmail.com>
2025-05-15 12:11:20 -0300

    test(product): adicionar testes unitários para UpdateProductHandler

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Unit/Application/TestData/UpdateProductHandlerTestData.cs
@@ -0,0 +1,19 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.UpdateProduct;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application.TestData;
+
+/// <summary>
+/// Provides methods to generate test data for UpdateProductHandler tests.
+/// </summary>
+public static class UpdateProductHandlerTestData
+{
+    private static readonly Faker<UpdateProductCommand> faker = new Faker<UpdateProductCommand>()
+        .RuleFor(p => p.Id, f => Guid.NewGuid())
+        .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+        .RuleFor(p => p.Description, f => f.Commerce.ProductDescription())
+        .RuleFor(p => p.UnitPrice, f => f.Random.Decimal(1, 10000))
+        .RuleFor(p => p.AvailableQuantity, f => f.Random.Number(1, 100));
+
+    public static UpdateProductCommand GenerateValidCommand() => faker.Generate();
+}
