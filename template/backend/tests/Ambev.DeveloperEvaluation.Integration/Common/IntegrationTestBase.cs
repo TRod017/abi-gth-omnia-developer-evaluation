a4deb4ef3bd3daea26eba783ee634cba27968944
Thiago Rodolfo <trodolfo@gmail.com>
2025-05-15 12:28:14 -0300

    test(integration): configurar infraestrutura de testes com WebApplicationFactory e SQLite in-memory

---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Integration/Common/IntegrationTestBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Integration;
+
+/// <summary>
+/// Base class for integration tests.
+/// </summary>
+public class IntegrationTestBase : IClassFixture<CustomWebApplicationFactory>
+{
+    protected readonly HttpClient Client;
+
+    public IntegrationTestBase(CustomWebApplicationFactory factory)
+    {
+        Client = factory.CreateClient();
+    }
+}
---
+++ template/backend/tests/Ambev.DeveloperEvaluation.Integration/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Ambev.DeveloperEvaluation.WebApi;
+
+namespace Ambev.DeveloperEvaluation.Integration;
+
+/// <summary>
+/// Custom factory for bootstrapping the application for integration testing without database.
+/// </summary>
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // TODO: Aqui você pode mockar dependências se necessário
+            // Exemplo: substituir repositórios por mocks com NSubstitute
+        });
+    }
+}
